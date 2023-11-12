42d46522d80958d419ccd1114cfdb1660b9d8d0a
Matt McMeans <mrmcmans@memphis.edu>
2023-11-11 18:21:36 -0600

    Quote is automatically rendered in tweet

--- random-quote-machine/src/App.js
+++ random-quote-machine/src/App.js
@@ -21,22 +21,29 @@ function App() {
     })
   }
 
+  const tweetQuoteUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(quoteInfo.text)} - ${encodeURIComponent(quoteInfo.author)}`;
+
   return (
-    <div className='d-flex align-items-center justify-content-center text-center min-vh-100' style={{ backgroundColor: '#5cbdea' }}>
+    <div className='d-flex row align-items-center justify-content-center text-center vh-100' style={{ backgroundColor: '#5cbdea' }}>
       <Card style={{ width: '60rem', height: 'auto', borderRadius: '15px' }}>
-        <Card.Body className='d-flex flex-column mt-auto justify-content-evenly align-items-center'>
+        <Card.Body className='d-flex flex-column justify-content-evenly align-items-center'>
           <Card.Title style={{ fontSize: '2rem' }}>Random Quote Generator</Card.Title>
           <div id="quote-box" className='text-center'>
-            <p id="text" className='mb-3' style={{ fontSize: '3rem', color: '#5cbdea' }}>"{quoteInfo.text}"</p>
+            <p id="text"  style={{ fontSize: '3rem', color: '#5cbdea' }}>"{quoteInfo.text}"</p>
             <p id="author" style={{ fontSize: '2rem' }}> - {quoteInfo.author}</p>
             <div className="row align-items-end justify-content-center">
               <button id="new-quote" onClick={getQuote} className='col-auto btn btn-primary mx-2'>New Quote</button>
-              <a href={"https://twitter.com/intent/tweet?hash..." + quoteInfo.text} id="tweet-quote" className='col-auto btn btn-info'>Tweet Quote</a>
+              <a href={tweetQuoteUrl} id="tweet-quote" className='col-auto btn btn-info' target="_blank" rel="noopener noreferrer">
+                Tweet Quote
+              </a>
             </div>
+            <p className='mx-auto' style={{ paddingTop: '10px' }}>By Matthew McMeans</p>
+
           </div>
         </Card.Body>
       </Card>
     </div>
+
   );
 }
 
